d11cf2c708d33eee7cd1c9336f032cc8db9d5448
xianyu-sheng <2107644732@qq.com>
2026-01-14 20:05:30 +0800

    fic

---
+++ build/CMakeFiles/kvcore.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xianyu-sheng/MyKV_storageBase_Raft_cpp/Clerk/clerk.cpp" "CMakeFiles/kvcore.dir/Clerk/clerk.cpp.o" "gcc" "CMakeFiles/kvcore.dir/Clerk/clerk.cpp.o.d"
+  "/home/xianyu-sheng/MyKV_storageBase_Raft_cpp/KvServer/KvServer.cpp" "CMakeFiles/kvcore.dir/KvServer/KvServer.cpp.o" "gcc" "CMakeFiles/kvcore.dir/KvServer/KvServer.cpp.o.d"
+  "/home/xianyu-sheng/MyKV_storageBase_Raft_cpp/Proto/raftKVRpcProtoc/KvServerRPC.pb.cc" "CMakeFiles/kvcore.dir/Proto/raftKVRpcProtoc/KvServerRPC.pb.cc.o" "gcc" "CMakeFiles/kvcore.dir/Proto/raftKVRpcProtoc/KvServerRPC.pb.cc.o.d"
+  "/home/xianyu-sheng/MyKV_storageBase_Raft_cpp/Proto/raftRpcProtoc/raftRPC.pb.cc" "CMakeFiles/kvcore.dir/Proto/raftRpcProtoc/raftRPC.pb.cc.o" "gcc" "CMakeFiles/kvcore.dir/Proto/raftRpcProtoc/raftRPC.pb.cc.o.d"
+  "/home/xianyu-sheng/MyKV_storageBase_Raft_cpp/Raft/Raft.cpp" "CMakeFiles/kvcore.dir/Raft/Raft.cpp.o" "gcc" "CMakeFiles/kvcore.dir/Raft/Raft.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/kvcore.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kvcore.dir/Clerk/clerk.cpp.o"
+  "CMakeFiles/kvcore.dir/Clerk/clerk.cpp.o.d"
+  "CMakeFiles/kvcore.dir/KvServer/KvServer.cpp.o"
+  "CMakeFiles/kvcore.dir/KvServer/KvServer.cpp.o.d"
+  "CMakeFiles/kvcore.dir/Proto/raftKVRpcProtoc/KvServerRPC.pb.cc.o"
+  "CMakeFiles/kvcore.dir/Proto/raftKVRpcProtoc/KvServerRPC.pb.cc.o.d"
+  "CMakeFiles/kvcore.dir/Proto/raftRpcProtoc/raftRPC.pb.cc.o"
+  "CMakeFiles/kvcore.dir/Proto/raftRpcProtoc/raftRPC.pb.cc.o.d"
+  "CMakeFiles/kvcore.dir/Raft/Raft.cpp.o"
+  "CMakeFiles/kvcore.dir/Raft/Raft.cpp.o.d"
+  "libkvcore.a"
+  "libkvcore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kvcore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
